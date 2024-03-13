21ced464d8b114024db95e8b2ada6009bb9da608
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-03-13 16:45:51 -0400

    Make base compiler modular.

--- casestudy/imp/Impzero.v
+++ casestudy/imp/Impzero.v
@@ -1,5 +1,10 @@
 (* A base family for Imp frontend languages *)
 family Impzero.Impcommon {
+    Inductive constant : Type :=
+        | Ointconst: int -> constant
+
+    Inductive unary_operation : Type := ...                              
+
     Inductive binary_operation : Type :=
         | Binplus
         | Binminus
@@ -7,7 +12,7 @@ family Impzero.Impcommon {
 
      Inductive expression : Type :=
         | Evar : ident -> expr 
-        | Econst : value -> expr
+        | Econst : constant -> expr
         | Ebinop : binary_operation -> expr -> expr -> expr.
 
      Inductive statement : Type :=
@@ -25,17 +30,21 @@ family Impzero.Impcommon {
     family Semantics {                                        
         Inductive cont : Type :=
            | Kstop : cont
-           | Kseq : statement -> cont -> cont
-           | Kwhile : expression -> statement -> cont -> cont
+           | Kseq : statement -> cont -> cont           
 
         Inductive state: Type :=
            | State: forall (s: statement) (k: cont) (e: env), state
-        
+
+        (* A common small step continuation-based semantics for frontned languages *)
         Inductive step : [self].state -> [self].state -> Prop :=
              | step_assign : forall st i a k n,            
                  aeval st a = n ->
                  step (State (Sassign i a) k st)
-                      (State Sskip k (t_update st i n))           
+                   (State Sskip k (t_update st i n))
+             (* | step_set: forall f id a k e le m v,
+                     eval_expr e le m a v -> (* TODO : fix this *)
+                     step (State (Sset id a) k e)
+                          (State Sskip k (PTree.set id v e)) *)                                    
              | step_seq : forall st c1 c2 k,  
                  step (State (Sseq c1 c2) k st)
                        (State c1 (Kseq c2 k) st)                             
@@ -46,8 +55,13 @@ family Impzero.Impcommon {
              | step_iffalse : forall st b c1 c2 k,
                  beval st b = false ->
                  step (State (Sifthenelse b c1 c2) k st)
-                       (State c2 k st)             
-             | step_skipseq: forall c k st,
+                   (State c2 k st)
+             (* | step_ifthenelse: forall a s1 s2 k e le m v b,
+                     eval_expr e le m a v ->
+                     Val.bool_of_val v b -> (* TODO: fix this *)
+                     step (State (Sifthenelse a s1 s2) k e)
+                          (State (if b then s1 else s2) k e) *)
+             | step_skip_seq: forall c k st,
                  step (State Sskip (Kseq c k) st)
                       (State c k st)                               
         
@@ -105,7 +119,12 @@ family Impzero.ImpcommonProofs {
   
    Inductive match_cont : A.Semantics.cont -> B.Semantics.cont -> Prop :=
      | match_Kstop: match_cont Imp.Semantics.Kstop Kstop                 
-                 
+     | match_Kseq: forall ce tyret nbrk ncnt s k ts tk,
+               transl_statement s = OK ts ->
+               match_cont k tk ->
+               match_cont (A.Semantics.Kseq s k) (B.Semantics.Kseq ts tk)
+
+                               
    Inductive match_states : A.Semantics.state -> B.Semantics.state -> Prop := ...
 
   
@@ -151,63 +170,42 @@ family Impzero.ImpcommonProofs {
      eexact translate_final_states.
      eexact translate_step.
    Qed.
-}                         
+}                        
 
-                         
-(* A toplevel program *)
-family Impzero.Imp.Program { }                         
-                         
-family Impzero {           
-   family Imp extends Impcommom {
-     (* Inherits expressions, binary_operaion, and statements *)
-       
-     Inductive statement : Type +=      
+(* Imp source language *)
+family Impzero.Imp extends Impcommom {        
+    Inductive statement : Type +=      
         | Swhile  : expression -> statement -> statement
 
-     family Semantics {
-         (* Inherits env *)
-         
-         Inductive cont : Type +=           
-           | Kwhile : expression -> statement -> cont -> cont
-        
-         (* Inherits state *)
+    family Semantics {            
+      Inductive cont : Type +=           
+        | Kwhile : expression -> statement -> cont -> cont              
 
-         Inductive step : [self].state -> [self].state -> Prop +=             
-             | KS_WhileTrue : forall st b c k,
-                 beval st b = true ->
-                 step (State (Swhile b c) k st)
-                       (State c (Kwhile b c k) st)
-             | KS_WhileFalse : forall st b c k,
-                 beval st b = false ->
-                 step (State (Swhile b c) k st)
-                       (State Sskip k st)
-             | KS_SkipWhile: forall b c k st,
-                 step (State Sskip (Kwhile b c k) st)
-                       (State (Swhile b c) k st)
-     }     
- }
-       
+      Inductive step : [self].state -> [self].state -> Prop +=             
+        | KS_WhileTrue : forall st b c k,
+            beval st b = true ->
+            step (State (Swhile b c) k st)
+                  (State c (Kwhile b c k) st)
+        | KS_WhileFalse : forall st b c k,
+            beval st b = false ->
+            step (State (Swhile b c) k st)
+                  (State Sskip k st)
+        | KS_SkipWhile: forall b c k st,
+            step (State Sskip (Kwhile b c k) st)
+                  (State (Swhile b c) k st)
+    }
+}                   
+                         
+family Impzero.Imp {
+   family Program { }
+}
 
- family Impsharpminor extends Impcommon {
-     Inductive constant : Type :=
-        | Ointconst: int -> constant       
-        | Ofloatconst: float -> constant   
-        | Osingleconst: float32 -> constant
-        | Olongconst: int64 -> constant.   
+(* Impsharpminor frontend IR *)
+family Impzero.Impsharpminor extends Impcommon {  
+     Inductive expression : Type +=                        
+        | Eunop : unary_operation -> expression -> expression
 
-     Inductive expression : Type :=
-        | Evar : ident -> expression                
-        | Eaddrof : ident -> expression             
-        | Econst : constant -> expression       
-        | Eunop : unary_operation -> expression -> expression  
-        | Ebinop : binary_operation -> expression -> expression -> expression
-        | Eload : memory_chunk -> expression -> expression. 
-
-     Inductive stmt : Type :=
-        | Sskip: stmt
-        | Sset : ident -> expr -> stmt        
-        | Sseq: stmt -> stmt -> stmt
-        | Sifthenelse: expr -> stmt -> stmt -> stmt        
+     Inductive stmt : Type +=                                
         | Sblock: stmt -> stmt
         | Sexit: nat -> stmt
         | Sswitch: bool -> expr -> lbl_stmt -> stmt
@@ -215,42 +213,20 @@ family Impzero {
 
         with lbl_stmt : Type :=
            | LSnil: lbl_stmt
-           | LScons: option Z -> stmt -> lbl_stmt -> lbl_stmt.
-
+           | LScons: option Z -> stmt -> lbl_stmt -> lbl_stmt
 
      family Semantics {
-         Inductive cont: Type :=
-            | Kstop: cont
-            | Kseq: stmt -> cont -> cont
-            | Kblock: cont -> cont      
-
-         (* States *)
-         Inductive state: Type :=
-             | State: forall (s: statement) (k: cont) (e: env), state
+         Inductive cont: Type +=            
+            | Kblock: cont -> cont         
                                                             
-            Inductive step: state -> state -> Prop :=
-                 | step_skip_seq: forall s k e,
-                     step (State Sskip (Kseq s k) e)
-                          (State s k e)
-                 | step_skip_block: forall k e,
+         Inductive step: state -> state -> Prop +=                
+               | step_skip_block: forall k e,
                      step (State Sskip (Kblock k) e)
-                          (State Sskip k e)                                
-                 | step_set: forall f id a k e le m v,
-                     eval_expr e le m a v -> (* TODO : fix this *)
-                     step (State (Sset id a) k e)
-                          (State Sskip k (PTree.set id v e))                 
-                 | step_seq: forall s1 s2 k e,
-                     step (State (Sseq s1 s2) k e)
-                          (State s1 (Kseq s2 k) e)        
-                 | step_ifthenelse: forall a s1 s2 k e le m v b,
-                     eval_expr e le m a v ->
-                     Val.bool_of_val v b -> (* TODO: fix this *)
-                     step (State (Sifthenelse a s1 s2) k e)
-                          (State (if b then s1 else s2) k e)              
-                 | step_loop: forall s k e le m,
+                          (State Sskip k e)                               
+               | step_loop: forall s k e le m,
                      step (State (Sloop s) k e)
                           (State s (Kseq (Sloop s) k) e)
-                 | step_block: forall f s k e,
+                | step_block: forall f s k e,
                      step (State (Sblock s) k e)
                           (State s (Kblock k) e)               
                 | step_exit_seq: forall n s k e,
@@ -267,53 +243,50 @@ family Impzero {
                     switch_argument islong v n -> (* TODO: evalutaion *)
                     step (State (Sswitch islong a cases) k e)
                          (State (seq_of_lbl_stmt (select_switch n cases)) k e)            
-               | step_label: forall lbl s k e,
+                | step_label: forall lbl s k e,
                    step (State (Slabel lbl s) k e)
                         (State s k e)               
-       }
+       }     
+}
 
-     (* Translation from Imp -> Impsharpminor *)
-       family Impshmgen extends {
-         (* This involves mostly simplification of control structures *)
-             Fixpoint translate_expression := ...
+(* Translation from Imp -> Impsharpminor *)
+family Impzero.Impshmgen {
+   (* This involves mostly simplification of control structures *)
+    Fixpoint translate_expression := ...
 
          
-             Fixpoint translate_statment := ...
+    Fixpoint translate_statment := ...
 
                                        
-         
-             family Proofs extends Impcommonproofs {
-                (*              
-                                     match_states
-                    Imp.state  ----------------------- Impsharpminor.state 
-                      |                                   |
-                      |                                   | *
-                      |                                   |
-                      v                                   v
-                   Imp.state' ----------------------- Impsharpminor.state'
-                                     match_states 
-                 *)
-              Inductive match_cont : Imp.Semantics.cont -> [self].Semantics.cont -> Prop :=
-                 | match_Kstop: match_cont Imp.Semantics.Kstop Kstop
-                 | match_Kseq: forall ce tyret nbrk ncnt s k ts tk,
-                       transl_statement s = OK ts ->
-                       match_cont k tk ->
-                       match_cont (Imp.Semantics.Kseq s k) (Kseq ts tk)
-                 (* | match_Kwhile *)
+   (* Correctness proofs for Imp -> Impsharpminor *)      
+   family Proofs extends Impcommonproofs {
+         (*              
+                           match_states
+           Imp.state  ----------------------- Impsharpminor.state 
+             |                                   |
+             |                                   | *
+             |                                   |
+             v                                   v
+          Imp.state' ----------------------- Impsharpminor.state'
+                          match_states 
+         *)
+      Inductive match_cont : Imp.Semantics.cont -> [self].Semantics.cont -> Prop +=                  
+         | match_Kwhile (* TODO: how does while relate to the semantics of Impsharpminor? *)
                  
-              Inductive match_states : Imp.Semantics.state -> [self].Semantics.state -> Prop :=
-                 | match_state:
-                     forall f nbrk ncnt s k e le m tf ts tk te ts' tk' cu,                         
-                         (TR: translate_statement cu.(prog_comp_env) nbrk ncnt s = OK ts)
-                         (MTR: match_transl ts tk ts' tk')
-                         (MK: match_cont k tk),
-                                match_states (Imp.Semantics.State s k e)
-                                             ([self].Semantics.State tf ts' tk' te le m)
-           }
+      Inductive match_states : Imp.Semantics.state -> [self].Semantics.state -> Prop +=
+         | match_state:
+             forall f nbrk ncnt s k e le m tf ts tk te ts' tk' cu,                         
+                 (TR: translate_statement cu.(prog_comp_env) nbrk ncnt s = OK ts)
+                 (MTR: match_transl ts tk ts' tk')
+                 (MK: match_cont k tk),
+                        match_states (Imp.Semantics.State s k e)
+                                     ([self].Semantics.State tf ts' tk' te le m)
+}
 
 
-    }
-
+  
+family Impzero {
+  
    family Impminor extends Impcommon {
 
    }
