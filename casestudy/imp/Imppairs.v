f972911d154d8b8932f2045315190e476e5d1235
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-03-14 01:08:58 -0400

    Start pairs.

--- casestudy/imp/Imppairs.v
+++ casestudy/imp/Imppairs.v
@@ -1,2 +1,11 @@
-(* Imp with pairs *) 
+(* Imp with pairs *)
+(* TODO: the eval inductive type *)
 family Imppairs extends Impzero { }
+
+family Imppairs.Impcommon {                 
+  Inductive expression : Type +=
+     | Epair : expr -> expr -> expr.
+  
+  
+
+}  
