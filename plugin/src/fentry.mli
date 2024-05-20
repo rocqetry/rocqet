ccef75452130f5ad55393ff0005076a96e3ce2a6
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-05-20 13:08:18 -0400

    Use the type signature

--- plugin/src/fentry.mli
+++ plugin/src/fentry.mli
@@ -2,6 +2,6 @@ val family : Names.Id.t -> unit
 
 val family_extends : derived:Names.Id.t -> base:Names.Id.t -> unit
 
-val finductive : (Vernacexpr.inductive_expr * Vernacexpr.decl_notation list) list -> unit 
+val finductive : Ftypes.VernacInductive.t list -> unit 
 
 val fend : Names.Id.t -> unit 
