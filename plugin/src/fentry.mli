9af3079edb2685022df31f622a8d1251b46fa2cb
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-05-20 13:11:42 -0400

    Fix type error

--- plugin/src/fentry.mli
+++ plugin/src/fentry.mli
@@ -2,6 +2,6 @@ val family : Names.Id.t -> unit
 
 val family_extends : derived:Names.Id.t -> base:Names.Id.t -> unit
 
-val finductive : Ftypes.VernacInductive.t list -> unit 
+val finductive : Ftypes.VernacInductive.t -> unit 
 
 val fend : Names.Id.t -> unit 
