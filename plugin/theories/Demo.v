4a405b665a47a912a7fa84a1a6523c5ddd24e795
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-05-20 21:54:06 -0400

    Parsing inductive types

--- plugin/theories/Demo.v
+++ plugin/theories/Demo.v
@@ -1,1 +1,15 @@
 From NFPOP Require Import Loader.
+
+Family Cminorvariant.
+    Family Semantics.
+    FEnd Semantics.
+FEnd Cminorvariant.
+
+Family Cminor extends Cminorvariant.
+    Family Semantics.
+       FInductive name := exec_step. 
+    FEnd Semantics.
+FEnd Cminor.
+
+(* We need to make the tool accessible from the `./casestudy` directory *)
+
