cf809d5a11f7292f33e214dc733995880f98a0d7
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-05-23 20:30:22 -0400

    Remove `termutils`.

--- compiler/src/ftermutil.mli
+++
@@ -1,8 +0,0 @@
-(* val global_env : unit -> Evd.evar_map * Environ.env
-
-val push_local : Names.Name.t Context.binder_annot * Evd.econstr -> Environ.env -> Environ.env
-
-val define : Names.variable -> Evd.econstr -> Evd.evar_map -> unit
-
-val internalize : Environ.env -> Constrexpr.constr_expr -> Evd.evar_map -> Evd.evar_map * Evd.econstr *)
-
