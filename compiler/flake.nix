c50b4f499f61088deb16015d3dac2934eb4f4c4d
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-05-27 18:07:09 -0400

    Add Nix flake

--- compiler/flake.nix
+++ compiler/flake.nix
@@ -1,1 +1,26 @@
-# A flake for reproducability
+{
+  description = "Nested family polymorphism in a proof language";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";    
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ ]; };
+        ocamlDeps = with pkgs.ocaml-ng.ocamlPackages_5_1; [
+          ocaml          
+          dune_3
+          ocamlformat_0_26_1
+          pkgs.coq
+        ];
+      in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = ocamlDeps;
+          };
+        }
+    );
+}
