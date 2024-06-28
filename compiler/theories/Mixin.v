cb5bebb94d933722d71324af60529ce8d4080564
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-06-28 14:29:50 -0400

    Update Mixin.

--- compiler/theories/Mixin.v
+++ compiler/theories/Mixin.v
@@ -7,6 +7,11 @@ Family STLCBase.
      | TUnit : Ty
      | TArr : Ty -> Ty -> Ty.
 
+  FRecursion subst about Ty motive (fun (_ : Ty) => nat) by _rec.
+       Case TUnit := 1.
+       Case TArr := (fun _ n _ m => m + n).
+   FEnd subst.
+
   FInductive Exp : Set :=     
      | EApp : Exp -> Exp -> Exp
      | EVal : Val -> Exp
@@ -20,6 +25,10 @@ Family IfExt.
    Family Base extends STLCBase.
        FInductive Ty : Set := TBool : Ty.
 
+       FRecursion subst.
+            Case TBool := 2.
+       FEnd subst.
+       
        FInductive Exp : Set :=
          | EIf : Exp -> Exp -> Exp -> Exp
        with Val : Set :=
@@ -34,6 +43,10 @@ FEnd IfExt.
 Family ArithExt. 
    Family Base extends STLCBase.
         FInductive Ty : Set := TNat : Ty.
+        
+        FRecursion subst.
+            Case TNat := 1.
+        FEnd subst.
 
        FInductive Exp : Set :=
          | EAdd : Exp -> Exp -> Exp
@@ -46,12 +59,18 @@ Family ArithExt.
 FEnd ArithExt.
 
 Family IfExtBuild extends IfExt.
-    Family Base extends STLCBase.
+    Family Base extends STLCBase.                    
+          (*FInductive B : Set := BB : Ty -> B.
+          FDefinition c := self__Base.B_rect.*)          
+          (* FInductive Ty : Set := Blah : Ty. *)
+          FRecursion subst.             
+          FEnd subst.
+          (* FDefinition A := Ty. *)
     FEnd Base.
 FEnd IfExtBuild.
 
 Family ArithExtBuild extends ArithExt.
-   Family Base extends IfExtBuild.Derived. 
+   Family Base extends IfExtBuild.Derived.       
    FEnd Base.
 FEnd ArithExtBuild.
 
