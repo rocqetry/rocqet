0c50fe6d045dc980584ec95ab4d2455cbfec744d
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-06-28 14:29:21 -0400

    Update STLC.v

--- compiler/theories/STLC.v
+++ compiler/theories/STLC.v
@@ -6,48 +6,48 @@ Family STLC.
   FInductive ty: Set :=
   | ty_unit : ty
   | ty_arrow : ty -> ty -> ty.
+     
+   FRecursion subst about ty motive (fun (_ : ty) => nat) by _rec.
+       Case ty_unit := 1.
+       Case ty_arrow := (fun _ n _ m => m + n).
+   FEnd subst.
 
-  Family ty_size_cases_handler.
-    FDefinition motive : self__STLC.ty -> Set := fun _ => nat.
-    FDefinition ty_unit :
-      self__ty_size_cases_handler.motive self__STLC.ty_unit
-      := 1.
-    FDefinition ty_arrow :
-      forall A, self__ty_size_cases_handler.motive A ->                
-      forall B, self__ty_size_cases_handler.motive B ->
-      self__ty_size_cases_handler.motive (self__STLC.ty_arrow A B)
-      := fun _ n _ m => n + m.
-  FEnd ty_size_cases_handler.
-
-  FRecursor ty_size_cases about ty
-    motive self__STLC.ty_size_cases_handler.motive
-    using self__STLC.ty_size_cases_handler
-    by _rec.
-
-  (* FInductive tm : Set :=
+  FInductive tm : Set :=
   | tm_var : ident -> tm
   | tm_app : tm -> tm -> tm
   | tm_val : val -> tm
   with val : Set :=
   | val_abs : ident -> tm -> val
-  | val_unit: val.*)
+  | val_unit: val.
 FEnd STLC.
 
-(* Family STLC_bool extends STLC.
+(* Print STLCsubstCases回17.*)
+
+Family STLC_bool extends STLC.
   FInductive ty : Set :=
     | ty_bool : ty.
+
+  FRecursion subst.
+      Case ty_bool := 1.
+  FEnd subst.    
 
   FInductive tm : Set :=
     | tm_if : tm -> tm -> tm -> tm
   with val : Set :=
     | val_true : val
     | val_false : val.
+
+  FDefinition check_handler_bool := substty_bool.
 FEnd STLC_bool.
 
 Family STLC_prod extends STLC_bool.
   FInductive ty : Set :=
     | ty_prod : ty -> ty -> ty.
 
+  FRecursion subst.
+     Case ty_prod := (fun _ n _ m => m + n).
+  FEnd subst.
+  
   FInductive tm : Set :=
     | tm_prod : tm -> tm -> tm
     | tm_pi1 : tm -> tm
@@ -55,4 +55,3 @@ Family STLC_prod extends STLC_bool.
   with val : Set :=
     | val_prod : val -> val -> val.
 FEnd STLC_prod.
-*)
