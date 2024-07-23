cbe9e7a1fe72ced75a1315cc5d804aab5573f895
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-07-23 17:30:43 -0400

    Add tactics to Loader.v

--- compiler/theories/Loader.v
+++ compiler/theories/Loader.v
@@ -1,1 +1,19 @@
+Ltac __unfold_ftheorem_motive := 
+  match goal with 
+  | [ |- ?h ?t] => try unfold h; try unfold t 
+  end.
+
+Ltac unfold_nested G :=
+  match G with 
+  | True => idtac
+  | (prod (?h ?a) ?G2)  => unfold h; unfold a; unfold_nested G2  
+  end.
+
+(* Need a recursive unfolding *)
+Ltac __unfold_ftheorem_motive_nested := 
+  match goal with 
+  | [ |- (prod ?a ?b) ] => unfold_nested (prod a b)
+  | _ => idtac
+  end.
+
 Declare ML Module "nfpop:nfpop.plugin".
