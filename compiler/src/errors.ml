f45677ce511e2eb8a3f1c2b2d46d1e7ac78f00c2
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-08-05 14:11:43 -0400

    Add a new function to Errors to print backtrace with errors

--- compiler/src/errors.ml
+++ compiler/src/errors.ml
@@ -10,4 +10,11 @@ exception ClosingWrongScope
 let report ~error = raise error
 let fail ~info = failwith info
 
+let failwith_stacktrace ~info = 
+  let backtrace =
+      Printexc.raw_backtrace_to_string @@ Printexc.get_callstack 10
+  in
+  let info = Printf.sprintf "Error: %s \n Stack Trace: %s" info backtrace in 
+  fail ~info
+
 (* TODO: Add functions for asserts *)
