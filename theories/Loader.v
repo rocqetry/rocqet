4e1a89e3627696a1c4d43282ea03c087db0eb450
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-09-10 00:28:12 -0400

    Add finduction tactics and unfold motives correctly

--- theories/Loader.v
+++ theories/Loader.v
@@ -1,19 +1,42 @@
+Ltac unfold_motive G :=
+  match G with 
+  | ?x => unfold x
+  | (?f ?x) => unfold x; unfold_motive f
+  end.
+
 Ltac __unfold_ftheorem_motive := 
   match goal with 
   | [ |- ?h ?t] => try unfold h; try unfold t 
+  | _ => idtac                                     
   end.
 
 Ltac unfold_nested G :=
   match G with 
   | True => idtac
   | (prod (?h ?a) ?G2)  => unfold h; unfold a; unfold_nested G2  
+  | ((?h ?a) /\ ?G2) => unfold_motive (h a); unfold_nested G2
   end.
 
 (* Need a recursive unfolding *)
 Ltac __unfold_ftheorem_motive_nested := 
   match goal with 
   | [ |- (prod ?a ?b) ] => unfold_nested (prod a b)
+  | [ |- (?a /\ ?b) ] => unfold_nested (a /\ b)
   | _ => idtac
+  end.
+
+(* Ltac __auto_split :=
+  match goal with
+  | [ |- prod _ _ ] => split; __unfold_ftheorem_motive_nested
+  | [ |- _ /\ _ ] => split; __unfold_ftheorem_motive_nested
+  | _ => idtac
+  end.*)
+
+Ltac finduction :=
+  match goal with
+  | [ |- ?a /\ ?b ] => 
+      split; finduction      
+  | _ => auto
   end.
 
 Declare ML Module "nfpop:nfpop.plugin".
