f746d349520c1505295d5e7743ad2a40f927df58
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-09-28 01:07:03 -0400

    Update test file

--- tests/Mutual_ind.v
+++ tests/Mutual_ind.v
@@ -2,18 +2,34 @@ From NFPOP Require Import Loader.
 
 Definition ident := nat.
 
-Inductive tm : nat -> Set :=
-  | tm_var : ident -> tm 0
-  | tm_app :  forall i j, tm i -> tm j -> tm 0
-  | tm_val : foo -> val -> tm 0                       
+Inductive tm : Set :=
+  | tm_var : ident -> tm
+  | tm_app :  tm  -> tm -> tm 
+  | tm_val : val -> tm 
   with val :  Set :=
-  | val_abs : ident -> tm 0 -> val
-  | val_unit: val
-  with foo : Set := Unit : tm 0 -> foo.
+  | val_abs : ident -> tm -> val
+  | val_unit: val.  
 
 Scheme tm_mut_rect := Induction for tm Sort Type
-with val_mut_rect := Induction for val Sort Type
-with foo_mut_rect := Induction for foo Sort Type.
+with val_mut_rect := Induction for val Sort Type.
+
+Check tm_mut_rect.
+Check tm_rect.
+
+tm_rect
+     : forall P : tm -> Type,
+       (forall i : ident, P (tm_var i)) ->
+       (forall t : tm, P t -> forall t0 : tm, P t0 -> P (tm_app t t0)) ->
+       (forall v : val, P (tm_val v)) -> forall t : tm, P t
+
+tm_mut_rect
+     : forall (P : tm -> Type) (P0 : val -> Type),
+       (forall i : ident, P (tm_var i)) ->
+       (forall t : tm, P t -> forall t0 : tm, P t0 -> P (tm_app t t0)) ->
+       (forall v : val, P0 v -> P (tm_val v)) ->
+       (forall (i : ident) (t : tm), P t -> P0 (val_abs i t)) -> P0 val_unit -> forall t : tm, P t
+
+(* Idea: use unit as a dummy argument *)
 
 (*Scheme tm_mut_rect := Induction for tm Sort Set
 with val_mut_rect := Induction for val Sort Set
