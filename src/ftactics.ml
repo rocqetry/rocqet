9a321265bb5e922b8731a964cd1f1369f2f53e1a
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-10-06 01:08:14 -0400

    Lookup computational axioms

--- src/ftactics.ml
+++ src/ftactics.ml
@@ -1,3 +1,5 @@
+open Env
+open Types
 (* Custom tactics for family polymorphism *)
 
 (* fsimpl *)
@@ -9,18 +11,52 @@ let fsimpl () =
     let env = Proofview.Goal.env gl in        
     let evar_map = Evd.from_env env in
 
-    let goal_names = Termutils.constants_in_econstr evar_map goal in     
-    (*let goal_names =
-      goal 
-      |> EConstr.to_constr evar_map                             
-      |> Termutils.reflect_checked_term                             
-      |> Constrexpr_ops.free_vars_of_constr_expr                           
-      |> Names.Id.Set.to_list                             
-    in *)
+    let goal_names = Termutils.constants_in_econstr evar_map goal in   
+    
+    let is_self_name name = 
+      name |> Names.Id.to_string |> String.starts_with ~prefix:"self__"
+    in 
+
+    let remove_self_qualid name = 
+       let rec remove_self_qual = function 
+           | [] -> Errors.fail ~info:"remove_self_qual: empty list"
+           | p :: path when is_self_name p -> Naming.list_to_path path
+           | _ :: path -> remove_self_qual path                             
+       in 
+       remove_self_qual (Naming.path_to_list name)
+    in 
+    
+    let is_self_qualid path = 
+      path |> Naming.path_to_list |> List.exists is_self_name
+    in
+
+    let computational_axioms = 
+      let context = Context.get () in 
+      goal_names
+      (* filter self names and extract the unqualified name *)                             
+      |> List.filter_map (fun name -> 
+            if is_self_qualid name then Some (remove_self_qualid name)
+            else None)
+      (* lookup linkage element *)                             
+      |> List.filter_map (fun name -> 
+           name |> Context.lookup_linkage_elem context |> Option.map fst)
+      (* extract inductive path *)                             
+      |> List.filter_map (function 
+           | LinkageElem.RecursorDefinition { names; handlers; _} -> Some (names, handlers)
+           | _ -> None)      
+      (* Now we create the computational axiom *)
+      |> List.concat_map (fun (names, handlers) -> 
+           let recursor_name = List.hd names in
+           handlers 
+           |> List.map (fun constructor_name -> 
+                  Naming.computational_axiom_name 
+                    ~recursor_name 
+                    ~constructor_name))           
+    in 
 
     let names = 
-      goal_names 
-      |> List.map Pretty.pretty_qualid
+      computational_axioms
+      |> List.map Names.Id.to_string
       |> String.concat "\n"                             
     in 
     
