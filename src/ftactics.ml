5a2ef0e294f521be618539868fcba5361495e63e
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-10-06 20:27:22 -0400

    fimpl in H

--- src/ftactics.ml
+++ src/ftactics.ml
@@ -110,7 +110,7 @@ let generate_unfolds unfold case_definitions =
    let all_unfold_tactics = List.map each_rewrite_tactic case_definitions in       
    let union_unfolds = 
      List.fold_right (fun l r -> Tacticals.tclTHEN l r) all_unfold_tactics idtac        
-   in      
+   in
    union_unfolds
 
 (* fsimpl *)
@@ -130,7 +130,7 @@ let fsimpl () =
     
     let rewrites = generate_rewrites computational_axioms in
       
-    let unfolds = generate_unfolds "__funfold" case_definitions in         
+    let unfolds = generate_unfolds "__funfold" case_definitions in
 
     let names = 
       computational_axioms
@@ -149,12 +149,6 @@ let fsimpl () =
     Tacticals.tclTHEN rewrites unfolds
   end
 
-
-(*let fsimpl_in (h: Names.Id.t) = 
-  Proofview.Goal.enter begin fun gl ->    
-    
-  end *)
-
 let fsimpl_star () =
   Proofview.Goal.enter begin fun gl ->    
     let goal = Proofview.Goal.concl gl in    
@@ -164,12 +158,8 @@ let fsimpl_star () =
       |> List.concat_map 
            (fun (binding : (Evd.econstr, Evd.econstr) Context.Named.Declaration.pt) -> 
              match binding with
-             | Context.Named.Declaration.LocalAssum ({ binder_name; _ }, ty) -> 
-                Feedback.msg_notice (Pp.str (Names.Id.to_string binder_name));                
-                [ty]
-             | Context.Named.Declaration.LocalDef ({ binder_name; _ }, tm, ty) -> 
-                Feedback.msg_notice (Pp.str (Names.Id.to_string binder_name));                
-                [tm;ty]) 
+             | Context.Named.Declaration.LocalAssum (_, ty) -> [ty]                
+             | Context.Named.Declaration.LocalDef (_, tm, ty) -> [tm;ty])
     in
     let env = Proofview.Goal.env gl in        
     let evar_map = Evd.from_env env in
@@ -187,6 +177,113 @@ let fsimpl_star () =
     let rewrites = generate_rewrites computational_axioms in   
       
     let unfolds = generate_unfolds "__funfold_star" case_definitions in   
+
+    let names = 
+      computational_axioms
+      |> List.map Pretty.pretty_qualid
+      |> String.concat "\n"                             
+    in 
+    Feedback.msg_info (Pp.str names) ;    
+
+    let names = 
+      case_definitions
+      |> List.map Pretty.pretty_qualid
+      |> String.concat "\n"                             
+    in    
+    Feedback.msg_info (Pp.str names) ;    
+    
+    Tacticals.tclTHEN rewrites unfolds
+  end
+
+let fsimpl_in h =
+  Proofview.Goal.enter begin fun gl ->        
+    let hyps : EConstr.t list = 
+      gl
+      |> Proofview.Goal.hyps 
+      |> List.filter_map
+           (fun (binding : (Evd.econstr, Evd.econstr) Context.Named.Declaration.pt) -> 
+             match binding with
+             | Context.Named.Declaration.LocalAssum ({ binder_name; _ }, ty) when Names.Id.equal binder_name h -> Some [ty]
+             | Context.Named.Declaration.LocalDef ({ binder_name; _ }, tm, ty) when Names.Id.equal binder_name h -> Some [tm;ty]
+             | _ -> None)                 
+      |> List.concat                             
+    in
+    let env = Proofview.Goal.env gl in        
+    let evar_map = Evd.from_env env in
+    
+    let hyps_names = hyps |> List.concat_map (Termutils.constants_in_econstr evar_map) in    
+    
+    let handlers = extract_handlers_names hyps_names in    
+
+    let computational_axioms = handlers_to_computational_axiom handlers in       
+    
+    let case_definitions = handlers_to_case_definitions handlers in
+    
+    let rewrites = 
+       let open Ltac_plugin in 
+      let each_rewrite_tactic (each_eq : Libnames.qualid) = 
+        let each_eq =
+          (Tacexpr.TacCall
+             (CAst.make
+                ( each_eq,
+                  [] )))      
+        in
+        let h =
+          (Tacexpr.TacCall
+             (CAst.make
+                ( Libnames.qualid_of_ident h,
+                  [] )))      
+        in
+        let tactic = 
+           CAst.make
+           (Tacexpr.TacArg
+              (Tacexpr.TacCall
+                 (CAst.make
+                    ( Libnames.qualid_of_ident
+                        (Names.Id.of_string "__frewrite_in"),
+                      [ each_eq; h ] ))))
+        in 
+        Tacinterp.interp tactic
+      in 
+      let all_rewrite_tactics = List.map each_rewrite_tactic computational_axioms in       
+      let union_rewrites = 
+        List.fold_right (fun l r -> Tacticals.tclTHEN l r) all_rewrite_tactics idtac        
+      in
+      union_rewrites
+   in 
+                             
+   let unfolds  = 
+      let open Ltac_plugin in 
+      let each_rewrite_tactic (each_eq : Libnames.qualid) = 
+        let each_eq =
+          (Tacexpr.TacCall
+             (CAst.make
+                ( each_eq,
+                  [] )))      
+        in
+        let h =
+          (Tacexpr.TacCall
+             (CAst.make
+                ( Libnames.qualid_of_ident h,
+                  [] )))      
+        in
+        let tactic = 
+           CAst.make
+           (Tacexpr.TacArg
+              (Tacexpr.TacCall
+                 (CAst.make
+                    ( Libnames.qualid_of_ident
+                        (Names.Id.of_string "__funfold_in"),
+                      [ each_eq; h ] ))))
+        in 
+        Tacinterp.interp tactic
+      in 
+      let all_unfold_tactics = List.map each_rewrite_tactic case_definitions in       
+      let union_unfolds = 
+        List.fold_right (fun l r -> Tacticals.tclTHEN l r) all_unfold_tactics idtac        
+      in
+      union_unfolds
+   in     
 
     let names = 
       computational_axioms
--- src/ftactics.mli
+++ src/ftactics.mli
@@ -1,2 +1,3 @@
 val fsimpl : unit -> unit Proofview.tactic
+val fsimpl_in : Names.Id.t -> unit Proofview.tactic
 val fsimpl_star : unit -> unit Proofview.tactic
