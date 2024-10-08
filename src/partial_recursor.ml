1ec0895cc21c00f6027f5ef0e37a644ae5f4daa7
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-10-08 12:47:10 -0400

    Move computational axiom generation to Termutils

--- src/partial_recursor.ml
+++ src/partial_recursor.ml
@@ -1,124 +1,6 @@
 open Env
 open Types
 
-(* (forall __i : self__Ix.ty,
- forall P : self__Ix.ty -> Type, forall arg回9 : option (P self__Ix.ty_unit), option (P __i)) *)
-
-let generate_one_computational_axiom 
-      ~(inductive: VernacInductive.t)      
-      ~(recursor_path: Libnames.qualid)
-      ~(constructor_name: Names.Id.t)
-      ~(constructor_path: Libnames.qualid)
-      ~(handlers: Names.Id.t list)
-      ~context =   
-  let open Constrexpr_ops in
-  let constructor_params, fully_applied_constr =
-    Termutils.extract_variables_and_apply (mkRefC constructor_path)
-  in
-  let extract_name ({ CAst.v = n; _ } : Names.lname) : Names.Id.t =
-      match n with
-      | Names.Name n -> n
-      | _ -> Errors.fail ~info:"Expected non anonymous argument"
-  in
-  (* The arguments to the consructor *)
-  let c_arguments =
-      constructor_params
-      |> List.map (fun ((lnames, _, _), _) ->
-             lnames |> List.hd |> extract_name 
-             (*|> Libnames.qualid_of_ident
-             |> mkRefC*))
-  in
-  (* The P predicate *)
-  let p_argument = Names.Id.of_string "P" in  
-  let generate_h_strings n =
-     let rec aux i acc =
-       if i > n then
-         List.rev acc
-       else
-         aux (i + 1) (("H" ^ string_of_int i) :: acc)
-     in
-     aux 1 []
-  in
-  let find_position x lst =
-     let rec aux current_pos = function
-       | [] -> Errors.fail ~info:"Handler not found"
-       | hd :: tl -> 
-           if Names.Id.equal hd x then current_pos
-           else aux (current_pos + 1) tl
-     in
-     aux 1 lst
-  in 
-  let position = find_position constructor_name handlers in
-  (* The "H" function of the current handler *)
-  let h_target = 
-    Names.Id.of_string (Printf.sprintf "H%d" position)
-  in
-  (* The H arguments *)
-  let h_arguments = 
-    handlers
-    |> List.length
-    |> generate_h_strings
-    |> List.map Names.Id.of_string
-  in
-  let partial_recursor_term arguments = 
-    let f = mkRefC recursor_path in 
-    mkAppC (f, arguments)
-  in
-  let equation_side arg = 
-    let p_and_hs = 
-      (p_argument :: h_arguments) 
-      |> List.map (fun n -> n |> Libnames.qualid_of_ident |> mkRefC) 
-    in
-    let arguments = arg :: p_and_hs in 
-    partial_recursor_term arguments
-  in
-  let left_hand_side = equation_side fully_applied_constr in
-  let right_hand_side = 
-    let types = 
-      Termutils.flatten_inductive_constructor_type 
-        ~inductive ~constructor:constructor_name
-    in
-    let f ty arg =      
-      match ty with
-      | None -> [ arg |> Libnames.qualid_of_ident |> mkRefC ]
-      | Some _ ->          
-          let arg = arg |> Libnames.qualid_of_ident |> mkRefC in
-          let rec_arg = equation_side arg in
-          [ arg; rec_arg ]
-    in
-    let arguments = List.concat (List.map2 f types c_arguments) in
-    let f = h_target |> Libnames.qualid_of_ident |> mkRefC in
-    mkAppC (f, arguments)
-  in 
-  let all_arguments = c_arguments @ [p_argument] @ h_arguments in
-  let equation = 
-    let eq_cstr = mkRefC @@ Libnames.qualid_of_ident @@ Names.Id.of_string "eq" in
-    mkAppC (eq_cstr, [ left_hand_side; right_hand_side ])
-  in 
-  let full_equation = Termutils.lambda_to_prod @@ Termutils.mk_lambda all_arguments equation in 
-  let family_name = context |> Context.family_name in
-  let name = Naming.prec_computational_axiom_name ~constructor_name ~family_name in
-  (name, full_equation)
-
-let generate_computational_axioms 
-    ~(inductive : VernacInductive.t) 
-    ~recursor
-    ~context ~prefix =   
-  let recursor_path = Libnames.qualid_of_ident recursor in
-  let constructors =
-    inductive |> List.hd |> fst |> VernacInductive.extract_type_and_cstrs |> snd
-    |> List.map fst
-  in
-  let handlers = constructors in
-  let constructors =
-    constructors
-    |> List.map (fun name -> (name, Naming.qualid_point prefix name))
-  in
-  constructors
-  |> List.map (fun (constructor_name, constructor_path) ->
-         generate_one_computational_axiom ~inductive ~context
-           ~recursor_path ~constructor_name ~constructor_path ~handlers)
-
 let add ~(inductive_path : Libnames.qualid) = 
   let context = Context.get () in    
   let default_ctx_params =
@@ -185,11 +67,11 @@ let add ~(inductive_path : Libnames.qualid) =
           let* _ = 
             thunk (fun () -> 
                 let axioms = 
-                  generate_computational_axioms
+                  Termutils.generate_prec_computational_axioms
                     ~inductive
-                    ~recursor:name 
+                    ~recursor_name:name 
                     ~context 
-                    ~prefix:(Some prefix) 
+                    ~prefix
                 in
                 results := axioms;
                 return ()) in 
