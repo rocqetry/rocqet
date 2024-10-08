a01df08b7159607f31d345a11bffdc87203454d1
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-10-07 22:16:16 -0400

    Implement todos and add tactics to Loader.v

--- src/partial_recursor.ml
+++ src/partial_recursor.ml
@@ -7,17 +7,30 @@ let add ~(inductive_path : Libnames.qualid) =
     context |> Context.family_linkage |> function
     | { default_ctx_params; _ } -> default_ctx_params
   in
-  let inductive_name = inductive_path |> Naming.path_to_list |> List.rev |> List.hd in 
-  (* TODO *)
-  let handlers = [] in
+  let inductive, _, _ =
+    Env.Context.lookup_inductive_for_recursion ~name:inductive_path context
+  in  
+  let handlers = 
+    inductive 
+    |> List.hd 
+    |> fst 
+    |> VernacInductive.extract_type_and_cstrs 
+    |> snd 
+    |> List.map fst 
+  in
+  let inductive_name = inductive_path |> Naming.path_to_list |> List.rev |> List.hd in   
   let family_name = Context.family_name context in
   let name = Naming.partial_recursor_name ~inductive_name ~family_name in
-  (* TODO *)
+  
   let type_name = Naming.fresh_name ~prefix:"PrecTy" in
+  let ty = Termutils.compute_partial_recursor_signature ~inductive_path ~context in  
+  let _ = Definition.add_definition ~name:type_name ty in
+  let context = Context.get () in
+  
   let compiled_context, parameters =
     Codegen.compile_linkage_context ~field_name:name context
   in
-  let ty = Termutils.compute_partial_recursor_signature ~inductive_path ~context in  
+  let ty = Constrexpr_ops.mkIdentC type_name in  
   let compiled_signature = 
     Codegen.compile_lemma_signature ~name ~ty ~parameters 
   in 
