561efba5f4f21f772d23e7b7ab5a11a745d92811
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-10-16 16:27:13 -0400

    Update "handler types" tests

--- tests/Handler_types.v
+++ tests/Handler_types.v
@@ -7,41 +7,43 @@ Definition float32 := nat.
 Definition ident := nat.
 
 Family Imp.
-   (*MetaData type.*)
-     FInductive type : Type :=
+   Family Common.
+   MetaData type.
+     Inductive type : Type :=
       | Tvoid: type(* the void type *).
-   (*FEnd type.*)
+   FEnd type.
     
-   (*MetaData val.*)
-      FInductive val: Type :=
+   MetaData val.
+      Inductive val: Type :=
         | Vundef: val
         | Vint: int -> val
         | Vlong: int64 -> val
         | Vfloat: float -> val
         | Vsingle: float32 -> val.
-   (*FEnd val.*)
+   FEnd val.
+   FEnd Common.
 
    Family I.
       FDefinition blah := nat.
       Family Clight.                    
           FInductive expr : Type :=          
-             | Econst_int: blah -> int -> type -> expr(* integer literal *)
-             | Econst_float: float -> type -> expr(* double float literal *)
-             | Econst_single: float32 -> type -> expr(* single float literal *)
-             | Econst_long: int64 -> type -> expr(* long integer literal *)                                            
-             | Etempvar: ident -> type -> expr (* temporary variable *)          
-             | Esizeof: type -> type -> expr (* size of a type *)
-             | Ecast: expr -> type -> expr
-             | Ealignof: type -> type -> expr. (* alignment of a type *)
+             | Econst_int: blah -> int -> Common.type -> expr(* integer literal *)
+             | Econst_float: float -> Common.type -> expr(* double float literal *)
+             | Econst_single: float32 -> Common.type -> expr(* single float literal *)
+             | Econst_long: int64 -> Common.type -> expr(* long integer literal *)                                            
+             | Etempvar: ident -> Common.type -> expr (* temporary variable *)          
+             | Esizeof: Common.type -> Common.type -> expr (* size of a type *)
+             | Ecast: expr -> Common.type -> expr
+             | Ealignof: Common.type -> Common.type -> expr. (* alignment of a type *)
       FEnd Clight.
    FEnd I.
 
    Family SimplExpr. 
-       FRecursion eval_simpl_expr about I.Clight.expr motive (fun (_ : I.Clight.expr) => option val) by _rect.
-          Case Econst_int := (fun _ n ty => Some(self__Imp.Vint n)).
-          Case Econst_float := (fun n ty => Some(self__Imp.Vfloat n)).          
-          Case Econst_single := (fun n ty => Some(self__Imp.Vsingle n)).
-          Case Econst_long := (fun n ty => Some(self__Imp.Vlong n)).
+       FRecursion eval_simpl_expr about I.Clight.expr motive (fun (_ : I.Clight.expr) => option Common.val) by _rect.
+          Case Econst_int := (fun _ n ty => Some(Common.Vint n)).
+          Case Econst_float := (fun n ty => Some(Common.Vfloat n)).          
+          Case Econst_single := (fun n ty => Some(Common.Vsingle n)).
+          Case Econst_long := (fun n ty => Some(Common.Vlong n)).
           Case Ecast := (fun b eval_simpl_expr_b ty  => 
                             match eval_simpl_expr_b with
                             | None => None
