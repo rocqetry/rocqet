c71bf324f12ac5be073253cd159aa2f0de66f41c
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-10-22 01:12:39 -0400

    Add tests

---
+++ tests/STLC_trait.v
@@ -0,0 +1,77 @@
+From NFPOP Require Import Loader.
+
+Definition ident := nat.
+
+Family STLC.
+  Trait unit_mixin extends X.
+  FInductive ty : Set :=
+  | ty_unit : ty.
+  FEnd unit_mixin.
+  
+  Family X extends unit_mixin.
+    FInductive ty : Set :=     
+     | ty_arrow : ty -> ty -> ty.
+  FEnd X.
+         
+  FRecursion subst : (t : X.ty) -> (k : nat) -> nat.  
+     Case ty_arrow n m := (subst m k + subst n k).
+     Case ty_unit := k.
+  FEnd subst.  
+  
+  FInduction subst_size
+       about X.ty
+       motive (fun (t : X.ty) => subst t 0 = 0).
+     FProof.  
+     - fsimpl. reflexivity. 
+     - intros. fsimpl in *. rewrite -> H. rewrite H0. 
+       reflexivity.
+     Qed. FEnd subst_size.  
+FEnd STLC.
+
+Family STLC_bool extends STLC.
+  
+  Trait bool_mixin extends X.  
+  FInductive ty : Set :=
+  | ty_bool : ty.
+  FEnd bool_mixin.    
+
+  Family X extends bool_mixin.  
+  FEnd X.
+
+  FRecursion subst.
+      Case ty_bool := 1.
+  FEnd subst.  
+
+  FInduction subst_size.    
+    FProof.
+      + apply cheat.
+    Qed.
+  FEnd subst_size.
+FEnd STLC_bool.
+
+Family STLC_prod extends STLC_bool.
+  Family X.
+    FInductive ty : Set :=
+      | ty_prod : ty -> ty -> ty.
+  FEnd X.
+
+  FRecursion subst.  
+     Case ty_prod n m := (subst m k + subst n k).
+  FEnd subst.
+
+  FInduction subst_size.     
+    FProof.
+    - intros. apply cheat.
+    Qed.
+  FEnd subst_size.  
+FEnd STLC_prod.
+
+Family STLC_nat. 
+  Family X.
+    FInductive ty : Set :=
+      | ty_nat : ty.
+  FEnd X.  
+FEnd STLC_nat.
+
+Family all extends STLC, STLC_bool, (*STLC_nat*) STLC_prod.
+FEnd all.
