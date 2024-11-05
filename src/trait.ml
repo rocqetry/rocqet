6619848aed476c5af8d6207045fc1d2e0132a698
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-11-05 00:41:26 -0500

    toplevel traits

--- src/trait.ml
+++ src/trait.ml
@@ -12,47 +12,72 @@ let lookup_base context base =
     match elem with 
     | Some (FamilyDefinition { linkage; _ }) -> Some linkage 
     | _ -> None
-    
+  
 
 let open_with_base ~name ~base = 
-  (* We don't have to inherit depenencies, we just need the compiled context *)
-  let context = Context.get () in  
-  let elem = lookup_base context base in   
-  let _, parameters =
-     Codegen.compile_linkage_context ~field_name:name context
-  in
-  let default_ctx_params =
-    Codegen.compile_default_params ~context:parameters
-  in
-  let base = 
-     match elem with 
-     | Some linkage -> 
-        let linkage =
-           Linkage.path_subtitution linkage
-             ~source:(Naming.self_version linkage.name)
-             ~target:(Naming.self_version name)
-        in
-        Some linkage
-     | _ -> Errors.fail ~info:"Unbound family name"
-  in 
-  let linkage =
-     Linkage.
-       {
-         context = Bwd.of_list parameters;
-         name;
-         definition = None;
-         base;
-         (* there's not point of using the real name here? *)
-         base_names = [];
-         fields = Bwd.Emp;
-         default_ctx_params;
-         signature = None;
-       }
-  in
-  let context = LinkageCtx.Nested (context, linkage) in
-  (* We want to delay structural checks until it is "mixed in" *)
-  (* Checks.check_further_binding_structure context; *)
-  Context.destructive_update (Some context)
+  (* We don't have to inherit depenencies, we just need the compiled context *)  
+  match Context.get_store () with 
+  | Some context -> 
+     let elem = lookup_base context base in
+     let _, parameters =
+        Codegen.compile_linkage_context ~field_name:name context
+     in
+     let default_ctx_params =
+       Codegen.compile_default_params ~context:parameters
+     in
+     let base = 
+        match elem with 
+        | Some linkage -> 
+           let linkage =
+              Linkage.path_subtitution linkage
+                ~source:(Naming.self_version linkage.name)
+                ~target:(Naming.self_version name)
+           in
+           Some linkage
+        | _ -> Errors.fail ~info:"Unbound family name"
+     in 
+     let linkage =
+        Linkage.
+          {
+            context = Bwd.of_list parameters;
+            name;
+            definition = None;
+            base;
+            (* there's not point of using the real name here? *)
+            base_names = [];
+            fields = Bwd.Emp;
+            default_ctx_params;
+            signature = None;
+          }
+     in
+     let context = LinkageCtx.Nested (context, linkage) in
+     (* We want to delay structural checks until it is "mixed in" *)
+     (* Checks.check_further_binding_structure context; *)
+     Context.destructive_update (Some context)
+  | None ->
+     let base = 
+        let path = Libnames.qualid_of_ident base in
+        match Context.lookup None path with 
+        | Some linkage -> Some linkage
+        | None -> Errors.fail ~info:"Unbound family name"
+     in
+     let linkage =
+        Linkage.
+          {
+            context = Bwd.Emp;
+            name;
+            definition = None;
+            base;
+            base_names = [];
+            fields = Bwd.Emp;
+            default_ctx_params = [];
+            signature = None;
+          }
+      in
+      let context = LinkageCtx.Toplevel linkage in
+      Context.destructive_update (Some context)
+
+
 
 let close_trait () =
   let context = Context.get () in
