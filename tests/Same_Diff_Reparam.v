687cd8a9f01a6e985944fb6d3ebe0ce11705542e
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-11-07 13:25:00 -0500

    Only apply parameters in context

---
+++ tests/Same_Diff_Reparam.v
@@ -0,0 +1,31 @@
+From NFPOP Require Import Loader.
+
+Family RV.
+
+Family Common.
+FDefinition a := 10.
+FEnd Common.
+
+Family RV32I extends Common.
+FDefinition b := 10.
+FDefinition c := 10.
+FEnd RV32I.
+
+Family RV64I extends RV32I.
+FDefinition d := 10.
+FDefinition e := 10.
+FEnd RV64I.
+
+FEnd RV.
+
+Family Comp.
+
+Family Inner.
+
+Family Asm extends RV.RV64I.
+FDefinition y := 10.
+FEnd Asm.
+
+FEnd Inner.
+
+FEnd Comp.
