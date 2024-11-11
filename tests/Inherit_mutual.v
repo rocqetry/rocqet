a76012fcc1806ec97141f024667547c56f006fdf
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-11-10 20:25:47 -0500

    Make Inherit work well for mutally inductive types

---
+++ tests/Inherit_mutual.v
@@ -0,0 +1,28 @@
+From NFPOP Require Import Loader.
+
+Family X.
+
+FDefinition condition := nat.
+FDefinition operation := nat.
+
+FInductive expr : Type :=
+| Econdition : condexpr -> expr -> expr -> expr
+| Eop : operation -> exprlist -> expr
+| Elet : expr -> expr -> expr
+| Eletvar : nat -> expr
+with exprlist : Type :=
+| Enil: exprlist
+| Econs: expr -> exprlist -> exprlist
+with condexpr : Type :=
+| CEcond : condition -> exprlist -> condexpr
+| CEcondition : condexpr -> condexpr -> condexpr -> condexpr
+| CElet: expr -> condexpr -> condexpr.
+
+
+FEnd X.
+
+Family Y extends X.
+
+Inherit expr.
+
+FEnd Y.
