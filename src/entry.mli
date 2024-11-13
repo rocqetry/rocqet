1cc1b56705e9bf9aaaea306a80f4138acffaca61
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2024-11-12 23:22:40 -0500

    traits multiple base

--- src/entry.mli
+++ src/entry.mli
@@ -93,6 +93,10 @@ val closing_fact :
 val inherit_name : name:Names.Id.t -> unit
 
 val open_trait_with_base : name:Names.Id.t -> base:Names.Id.t -> unit
+val open_trait_with_base_list:
+  name:Names.Id.t ->
+  bases:Names.Id.t list ->
+  unit
 val open_trait : name:Names.Id.t -> unit
 
 val final_family : 
