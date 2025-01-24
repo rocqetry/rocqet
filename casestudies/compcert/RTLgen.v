3feaba84cf4744f4179c79563360c24a3ba5cd96
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2025-01-23 19:05:44 -0500

    prove Ebuiltin

--- casestudies/compcert/RTLgen.v
+++ casestudies/compcert/RTLgen.v
@@ -3050,7 +3050,7 @@ FInductive step: genv -> state -> trace -> state -> Prop :=
 FEnd RTL.
 
 Family RTLgen.
-Family So extends CminorSel. FEnd S.
+Family S extends CminorSel. FEnd S.
 Family T extends RTL. FEnd T.
 
 FDefinition exprlist_of_expr_list : list S.expr -> S.exprlist := fun l =>
@@ -3188,8 +3188,15 @@ FInductive tr_stmt : T.code -> mapping -> S.stmt -> T.node -> T.node -> list T.n
 
 FInduction transl_expr_charact with transl_exprlist_charact with transl_condexpr_charact.
 FProof.
+all : intros; fsimpl in TR; try (monadInv TR); saturateTrans.
 (* Ebuiltin *)
-+ apply cheat.
++ inv OK.
+  - apply cheat. - apply cheat. 
+  - fconstructor; eauto with rtlg.
+    eapply H; eauto with rtlg.
+    eapply alloc_regs_target_ok; eauto with rtlg.
+    simple eapply regs_valid_incr. exact INCR2. 
+    simple eapply alloc_regs_valid. exact WF. exact EQ.
 Qed. FEnd transl_expr_charact with transl_exprlist_charact with transl_condexpr_charact.
 
 FInduction transl_expr_assign_charact.
