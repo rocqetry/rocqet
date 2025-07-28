674d67a007c5b846f67e83e1ef7ae618a7500bdd
Oghenevwogaga Ebresafe <oghenevwogaga.ebresafe@uwaterloo.ca>
2025-07-28 01:37:14 -0400

    Ask a question about default values and use the syntax

--- tests/Records_test.v
+++ tests/Records_test.v
@@ -12,9 +12,10 @@ Definition x := {| id := 0 |}.
 Definition y := Build_lambda_arg 0.*)
 
 Family STLC.
+
 FInductive ty: Set :=
-  | ty_unit : ty
-  | ty_arrow : ty -> ty -> ty.
+| ty_unit : ty
+| ty_arrow : ty -> ty -> ty.
 
 FRecord lambda_arg : Set := { id : ident }.
 
@@ -22,45 +23,52 @@ FDefinition x : lambda_arg := Build_lambda_arg_STLC 10.
 
 FDefinition y : ident := id (Build_lambda_arg_STLC 10).
 
-FEnd STLC.
+FLemma easy_lemma : id (Build_lambda_arg_STLC 10) = 10.
+FProofLemma.
+fsimpl.
+Qed. CloseFLemma.
 
-(*MetaData _id_comp.
-Axiom id : lambda_arg ->  ident.
-FEnd _id_comp.
 
+(*
 MetaData lambda_arg_eq_STLC.
 Axiom axiom_id : forall x, id (Build_lambda_arg_STLC x) = x.
-FEnd lambda_arg_eq_STLC.*)
+FEnd lambda_arg_eq_STLC.
+*)
 
 FInductive tm : Type :=
-  | tm_var : ident -> tm    
-  | tm_abs : lambda_arg -> tm -> tm
-  | tm_app : tm -> tm -> tm
-  | tm_unit: tm.
+| tm_var : ident -> tm    
+| tm_abs : lambda_arg -> tm -> tm
+| tm_app : tm -> tm -> tm
+| tm_unit: tm.
 
 FEnd STLC.
 
 
-Family SystemF extends STLC. 
+Family SystemF extends STLC.
+
 FDefinition tvar := nat.
 
 FInductive ty: Set :=
 | ty_forall : tvar -> ty -> ty. (* ∀α.T *)
 
 (* tm_abs is extended with a type *)
-FRecord lambda_arg : Set := {
-    arg_ty : ty;
-  }.
-FDefault lambda_arg arg_ty := ty_unit.
+FRecord lambda_arg : Set := { arg_ty : ty } with default arg_ty := ty_unit.
+
 
 MetaData lambda_arg_constr_SystemF.
 Axiom Build_lambda_arg_SystemF : ident -> ty -> lambda_arg.
 FEnd lambda_arg_constr_SystemF.
 
+(* Is it possible to modularly compose two records? We need a way to supply the
+   default value. A couple of options:
+   - Allow field to have default values regardless of whether we're extending
+     a field or not
+   - Allow to supply the default value like a proof obligation -- similar to
+     FRecursion/FInduction *)
 MetaData lambda_arg_eq_STLC.
 Axiom axiom_id0 : forall i t, arg_ty (Build_lambda_arg_SystemF i t) = t.
 Axiom axiom_id1 : forall i, (Build_lambda_arg_STLC i) = (Build_lambda_arg_SystemF i ty_unit).
-FEnd lambda_arg_eq_STLC.*)
+FEnd lambda_arg_eq_STLC.
 
 
 FInductive tm : Type :=  
